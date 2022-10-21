0513bbc6e1439f121f83a8e3407e9596d4f5cffb
Sumukh-Robosoft <sumukh.r@robosoftin.com>
2022-10-21 17:53:21 +0530

    refresh token

--- controller/signin.js
+++ controller/signin.js
@@ -4,6 +4,7 @@ const JWT = require("jsonwebtoken")
 const fast2sms = require('fast-two-sms');
 const cookieparser = require('cookie-parser');
 const bodyParser = require('body-parser');
+require("dotenv").config()
 
 const signin = async (req,res) =>{
     const phoneNumber= req.body.phoneNumber;
@@ -39,35 +40,30 @@ await UserModel.findOneAndUpdate({phoneNumber:req.body.phoneNumber},{
 }
 
 const refresh = async(req,res)=>{
-    try {
-        const cookies = req.cookies.JWT;
-        if (!cookies) return res.sendStatus(401);
     
-        const refreshToken = cookies;
-    
-        UserModel.findOne({token }, async (err, docs) => {
-          if (err)
-          
-          return res.status(400).json({ status: 400, errors: errors.array() });
-         if(docs) {
-            JWT.verify(
-              refreshToken,
-              process.env.REFRESH_TOKEN_SECRET,
-              (err, decoded) => {
-                if (err || docs.token !==decoded.token ) return res.sendStatus(403);
-                const token = JWT.sign(
-                  { phoneNumber:req.body.phoneNumber },
-                  process.env.SECRET_CODE,
-                  { expiresIn: "60m" }
-                );
-             console.log(token);
-              }
-            );
-          }
-        });
-      } catch (error) {
-        res.send(error)
-      }
+    if (req.cookies?.JWT) {
+        const refreshToken =UserModel.findOne(req.body.phoneNumber);
+
+        JWT.verify(refreshToken.token, process.env.REFRESH_TOKEN_SECRET, 
+        (err, decoded) => {
+            if (err) {
+
+                return res.status(406).json({ message: 'Unauthorized' });
+            }
+            else {
+                
+                const token = JWT.sign({
+                 phoneNumber: req.body.phoneNumber,
+                    
+                }, process.env.SECRET_CODE, {
+                    expiresIn: '10m'
+                });
+                return res.json({ token });
+            }
+        })
+    } else {
+        return res.status(406).json({ message: 'Unauthorized' });
+    }
 }
     
 
